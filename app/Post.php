d469f45c6c015169d8fd9ab2ec255f25a63b9eae
Matovu Ronald <matovuronald997@gmail.com>
2019-01-03 16:08:34 +0300

    Add body and excerpt html accessors

--- app/Post.php
+++ app/Post.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Carbon\Carbon;
+use Markdown;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
@@ -30,6 +31,16 @@ class Post extends Model
     public function getDateAttribute($value)
     {
         return is_null($this->published_at) ? '' : $this->published_at->diffForHumans();
+    }
+
+    public function getBodyHtmlAttribute($value)
+    {
+        return $this->body ? Markdown::convertToHtml(e($this->body)) : NULL;
+    }
+
+    public function getExcerptHtmlAttribute($value)
+    {
+        return $this->excerpt ? Markdown::convertToHtml(e($this->excerpt)) : NULL;
     }
 
     public function scopeLatestFirst($query)
