3fc6eb8191b92d0118c5fcca50161cf11f260af0
Matovu Ronald <matovuronald997@gmail.com>
2019-01-03 07:09:09 +0300

    Create Post Model
    That is Modal, Migration , Controller

---
+++ database/migrations/2019_01_03_035552_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('author_id');
+            $table->foreign('author_id')->references('id')->on('users')->onDelete('restrict');
+            // $table->foreign('author_id')->references('id')->on('users')->onDelete('restrict');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('excerpt');
+            $table->text('body')->nullable();
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
