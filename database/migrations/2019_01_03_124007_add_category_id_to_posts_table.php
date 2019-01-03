fbf330010ea2705a79569fd799cf6592628d4c55
Matovu Ronald <matovuronald997@gmail.com>
2019-01-03 16:10:53 +0300

    Alter Posts table, add category_id column

---
+++ database/migrations/2019_01_03_124007_add_category_id_to_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCategoryIdToPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->unsignedInteger('category_id')->after('author_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('category_id');
+        });
+    }
+}
