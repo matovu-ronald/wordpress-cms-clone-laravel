c619e4a842c4b172f8dbbe8b4e84a96980a9ff24
Matovu Ronald <matovuronald997@gmail.com>
2019-01-03 09:06:05 +0300

    Create dummy data for posts and users

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+use Faker\Generator as Faker;
+use Faker\Provider\tr_TR\DateTime;
+
+
+
+$factory->define(App\Post::class, function( Faker $faker ) {
+    return [
+        'author_id' => function () {
+            if (rand(1, 10) % 5 == 0) {
+                return factory(App\User::class)->create()->id;
+            } else {
+                return rand(1, 5);
+            }
+        },
+        'title' => $faker->sentence(rand(8, 12)),
+        'excerpt' => $faker->text(rand(250, 300)),
+        'body' => $faker->paragraphs(rand(10, 15), true),
+        'slug' => $faker->slug(),
+        'image' => function () {
+            return rand(0, 1) == 1 ? ('Post_Image_' . rand(1, 5) . '.jpg') : NULL;
+        },
+        'created_at' => $faker->date(),
+        'updated_at' => $faker->date()
+    ];
+});
---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class, 10)->create();
+        factory(App\Post::class, 20)->create();
+    }
+}
