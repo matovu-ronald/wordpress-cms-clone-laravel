59f1b5285766becc0d624fae734a78c49b89b402
Matovu Ronald <matovuronald997@gmail.com>
2019-01-04 16:54:49 +0300

    Move HomeController to Backend folder for controllers

---
+++ app/Http/Controllers/Backend/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use Illuminate\Http\Request;
+
+class HomeController extends BackendController
+{
+   
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
