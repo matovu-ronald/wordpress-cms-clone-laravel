81227d00d781e7b78df05276dbeae0caf1cfb99e
Matovu Ronald <matovuronald997@gmail.com>
2019-01-04 16:55:20 +0300

    Override logout method to login page

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,6 +27,7 @@ class LoginController extends Controller
      */
     protected $redirectTo = '/home';
 
+
     /**
      * Create a new controller instance.
      *
@@ -35,5 +36,10 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function loggedOut()
+    {
+        return redirect()->route('login');
     }
 }
