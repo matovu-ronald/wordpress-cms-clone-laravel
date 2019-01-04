4b6a88c74bdacac41201b6db4758b0040e5da7dd
Matovu Ronald <matovuronald997@gmail.com>
2019-01-04 03:52:30 +0300

    Add Markup for Search Box

--- resources/views/blog/sidebar.blade.php
+++ resources/views/blog/sidebar.blade.php
@@ -1,9 +1,14 @@
 <aside class="right-sidebar">
-    <div class="search-widget">
-
+    <div class="search-widget shadow-sm my-4 p-4">
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search For ..." aria-label="search" aria-describedby="search">
+            <div class="input-group-append">
+                <i class="input-group-text fa fa-search" id="search"></i>
+            </div>
+        </div>
     </div>
-    <div class="widget">
-        <ul class="list-group">
+    <div class="widget shadow-sm">
+        <ul class="list-group list-group-flush">
             @foreach ($categories as $category)
                 <li class="list-group-item d-flex justify-content-between align-items-center">
                    <a href="{{ route('category.show', $category->slug) }}">{{ $category->title }}</a> 
